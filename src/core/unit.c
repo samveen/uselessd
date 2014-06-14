acb194d6fa0921f3bbdc2c1f2f42c3713d5f8238
The Initfinder General <boycottsystemd@openmailbox.org>
2014-06-13 23:06:10 -0400

    core: replace _NSIG instances with SIGRTMIN as a workaround

--- src/core/unit.c
+++ src/core/unit.c
@@ -2543,7 +2543,7 @@ int unit_kill(Unit *u, KillWho w, int signo, DBusError *error) {
         assert(u);
         assert(w >= 0 && w < _KILL_WHO_MAX);
         assert(signo > 0);
-        assert(signo < _NSIG);
+        assert(signo < SIGRTMIN); // __NSIG == SIGRTMIN
 
         if (!UNIT_VTABLE(u)->kill)
                 return -ENOTSUP;
