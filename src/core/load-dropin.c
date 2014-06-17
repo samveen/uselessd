8675656d411da883d0abd3b45ac0c7f3351c570f
The Initfinder General <boycottsystemd@openmailbox.org>
2014-06-16 21:23:49 -0400

    load-dropin: comment out foreach iterator with the load_fragment_gperf_lookup() call

--- src/core/load-dropin.c
+++ src/core/load-dropin.c
@@ -186,11 +186,11 @@ int unit_load_dropin(Unit *u) {
         if (! u->dropin_paths)
                 return 0;
 
-        STRV_FOREACH(f, u->dropin_paths) {
+        /*STRV_FOREACH(f, u->dropin_paths) {
                 config_parse(u->id, *f, NULL,
                              UNIT_VTABLE(u)->sections, config_item_perf_lookup,
                              (void*) load_fragment_gperf_lookup, false, false, u);
-        }
+        }*/
 
         u->dropin_mtime = now(CLOCK_REALTIME);
 
