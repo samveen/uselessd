9e2406dc4bd612db0bcb4a72158e7bc26ad1f3fd
The Initfinder General <boycottsystemd@openmailbox.org>
2014-11-24 15:16:06 -0500

    util: indent in is_locale_utf8()

--- src/shared/util.c
+++ src/shared/util.c
@@ -5388,7 +5388,7 @@ bool is_locale_utf8(void) {
                 goto out;
         }
 
-        if(streq(set, "UTF-8")) {
+        if (streq(set, "UTF-8")) {
                 cached_answer = true;
                 goto out;
         }
