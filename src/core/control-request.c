e28d40a40d7bb097462129a54d94f821e6532562
The Initfinder General <boycottsystemd@openmailbox.org>
2014-11-26 21:50:55 -0500

    control-request: add disable

--- src/core/control-request.c
+++ src/core/control-request.c
@@ -243,11 +243,44 @@ void fifo_control_loop(void) {
 
                 /* TODO: arg-force and carries_install_info.
                  * Fix encoding in UnitFileChange logging,
-                 * or remove entirely. */
+                 * or remove entirely. Put stuff into
+                 * functions in control-response-util. */
                 } else if (streq("enab", fifobuf)) {
                         break;
                 } else if (streq("disa", fifobuf)) {
-                        break;
+                        int disable;
+                        int unit_to_disable;
+                        const char *argroot;
+                        UnitFileScope argscope;
+                        bool argruntime;
+                        const char *p = NULL;
+
+                        UnitFileChange *changes;
+                        unsigned n_changes = 0, ic;
+
+                        argroot = get_arg_root();
+                        if (streq("unknown", argroot))
+                                log_error("Failed to get unit file root from /run/systemd/arg-root.");
+
+                        argruntime = test_is_runtime();
+
+                        argscope = get_arg_scope();
+                        if (argscope < 0)
+                                log_error("Failed to get unit file scope from /run/systemd/arg-scope.");
+
+                        unit_to_disable = read_one_line_file("/run/systemd/manager/disable", (char **)&p);
+                        if (unit_to_disable < 0)
+                                log_error("Failed to disable unit file: %s.", strerror(-unit_to_disable));
+
+                        disable = unit_file_disable(argscope, argruntime, argroot, (char **)p, &changes, &n_changes);
+
+                        for (ic = 0; ic < n_changes; ic++) {
+                                if (changes[ic].type == UNIT_FILE_SYMLINK)
+                                        log_info("ln -s '%s' '%s'", changes[ic].source, changes[ic].path);
+                                else
+                                        log_info("rm '%s'", changes[ic].path);
+                        }
+
                 } else if (streq("isen", fifobuf)) {
                         UnitFileState state;
                         UnitFileScope argscope;
