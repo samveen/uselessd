445456caefc24a86d6383b64c02a20a84af7715f
The Initfinder General <boycottsystemd@openmailbox.org>
2014-12-01 18:24:42 -0500

    control-request: add suspend

--- src/core/control-request.c
+++ src/core/control-request.c
@@ -384,6 +384,9 @@ void fifo_control_loop(void) {
                 } else if (streq("emerg", fifobuf)) {
                         Job *j;
                         manager_add_job_by_name(m, JOB_START, SPECIAL_EMERGENCY_TARGET, JOB_ISOLATE, true, &j);
+                } else if (streq("suspn", fifobuf)) {
+                        Job *j;
+                        manager_add_job_by_name(m, JOB_START, SPECIAL_SUSPEND_TARGET, JOB_REPLACE_IRREVERSIBLY, true, &j);
                 } else if (streq("swirt", fifobuf)) {
                         break;
                 } else if (streq("resfa", fifobuf)) {
