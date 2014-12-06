b43f209dc9f58d0da33654bdc17e44458c9de685
The Initfinder General <boycottsystemd@openmailbox.org>
2014-12-06 12:07:34 -0500

    unit: remove bus-errors header

--- src/core/unit.c
+++ src/core/unit.c
@@ -46,7 +46,6 @@
 #include "mkdir.h"
 #include "label.h"
 #include "fileio-label.h"
-#include "bus-errors.h"
 
 const UnitVTable * const unit_vtable[_UNIT_TYPE_MAX] = {
         [UNIT_SERVICE] = &service_vtable,
